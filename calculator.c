ce4e329da86ca9c8b258042b2b27c9808dd028e7
jsampaiof <jostherfontes@hotmail.com>
2022-05-25 23:01:05 -0300

    4 operações

--- calculator.c
+++ calculator.c
@@ -5,9 +5,9 @@ int main(){
 	setlocale(0,"portuguese");
     float number1, number2, result;
     int operation;
-    
+    inicio:
     printf("O que você quer calcular? 1-Soma; 2-subtração; 3-multiplicação; 4-divisão/What do you want to calculate? 1-Sum; 2-subtraction; 3-multiplication; 4-division \n");
-    printf("Digite abaixo o número referente a operação desejada/Enter the number for the desired operation below \n");
+    printf("Digite abaixo o número referente a operação desejada:/Enter the number for the desired operation below: \n");
     scanf("%d", &operation);
 
     printf("qual o primeiro número?/whats the first number? \n");
@@ -19,27 +19,28 @@ int main(){
     switch(operation){
     	case 1:
     	result = number1 + number2;
-    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f", result);
+    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f \n", result);
     	break;
     	case 2:
     	result = number1 - number2;
-    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f", result);
+    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f \n", result);
     	break;
 		case 3:
     	result = number1 * number2;
-    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f", result);
+    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f \n", result);
     	break;
 		case 4:
     	result = number1 / number2;
-    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f", result);
+    	printf("o resultado da ação escolhida é:/The result for the chosen action is: %f \n", result);
     	break;
 	}
-	printf("deseja continuar ou sair? Digite 1 para continuar ou 2 para sair: \n");
+	printf("deseja continuar ou sair? Digite 1 para continuar ou 2 para sair:/Do you wanna keep or leave? Type 1 to keep or 2 to leave: \n");
 	scanf("%d", &operation);
 	if(operation==1){
-		goto
+		system("cls");
+		goto inicio;
 	}
-	else(operation==2){
-		
+	else(operation==2);{
+		return 0;
 	}
 }
